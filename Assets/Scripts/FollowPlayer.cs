cdc5b93d2a2722df8ef969ba9a31a0caf8f80d55
PharingWell <pharingwell@gmail.com>
2025-06-22 22:09:21 -0400

    Adjusted camera properties

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
-    private Vector3 offset = new Vector3(0f, 20f, -3f); // Adjust as needed
+    private Vector3 offset = new Vector3(0f, 5f, -5f); // Adjust as needed
 
-    private float rotationSmoothSpeed = 2f;
+    private float rotationSmoothSpeed = 3f;
 
     void FixedUpdate()
     {
         transform.position = player.position + player.rotation * offset;
 
-        Quaternion targetRotation = Quaternion.Euler(60f, player.eulerAngles.y, 0f);
+        Quaternion targetRotation = Quaternion.Euler(30f, player.eulerAngles.y, 0f);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSmoothSpeed * Time.deltaTime);
     }
 }
